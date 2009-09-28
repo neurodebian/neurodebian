85be2cd5d4f45585c585366165fc6f2dd81313f3
Michael Hanke <michael.hanke@gmail.com>
2009-09-28 13:38:30 -0400

    Tiny changes to livecd script.

--- tools/makelivecd.sh
+++ tools/makelivecd.sh
@@ -1,3 +1,7 @@
+#!bin/sh
+
+set -e
+
 # for a fresh build one might want to run:
 # sudo rm -rf cache/stages_bootstrap chroot/ config/ scripts/ binary* .stage
 
@@ -23,21 +27,29 @@ lh_config \
   --binary-images iso \
   --binary-indices none \
   --bootstrap-flavour minimal \
+  --categories "main contrib non-free" \
   --debian-installer disabled \
   --distribution ${nd_basedist} \
   --hostname debian \
   --iso-application NeuroDebian \
-  --iso-publisher "Debian Experimental Psychology Project; http://alioth.debian.org/projects/pkg-exppsy" \
-  --mirror-bootstrap "http://ftp.uni-magdeburg.de/aftp/projects/linux/debian/" \
-  --packages-lists lxde \
-  --categories "main contrib non-free" \
+  --iso-publisher "NeuroDebian; http://neuro.debian.org" \
+  --mirror-bootstrap "http://debian.lcs.mit.edu/debian/" \
+  --packages-lists xfce \
+  --packages "${nd_packages}" \
   --username neuro \
-  --packages "${nd_packages}"
+  --win32-loader enabled
 
 # add the NeuroDebian repository to the APT setup
-echo "deb http://apsy.gse.uni-magdeburg.de/debian ${nd_basedist} main contrib non-free" \
+echo "deb http://neuro.debian.net/debian ${nd_basedist} main contrib non-free" \
     > config/chroot_sources/neurodebian.chroot
 cp config/chroot_sources/neurodebian.chroot config/chroot_sources/neurodebian.binary
 # and the key
 gpg --export -a ${nd_key} > config/chroot_sources/neurodebian.chroot.gpg
 cp config/chroot_sources/neurodebian.chroot.gpg config/chroot_sources/neurodebian.binary.gpg
+
+
+# use NeuroDebian package cache
+if [ ! -e "cache/packages_bootstrap" ]; then
+  mkdir -p cache
+  ln -s ../../debian_aptcache/ cache/packages_bootstrap
+fi
