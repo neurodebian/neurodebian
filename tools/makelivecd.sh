4a6658e1129a59ee5f1da129ab94baf3f4bbe11c
Yaroslav Halchenko <debian@onerussian.com>
2010-11-22 23:40:14 -0500

    initial tune ups in makelivecd.sh so it works (?) with new live-build system

--- tools/makelivecd.sh
+++ tools/makelivecd.sh
@@ -6,50 +6,67 @@ set -e
 # sudo rm -rf cache/stages_bootstrap chroot/ config/ scripts/ binary* .stage
 
 # the Debian release to use as a basis for the live-cd
-nd_basedist="lenny"
+nd_basedist="squeeze"
 # the NeuroDebian archive key
 nd_key="A5D32F012649A5A9"
 # packages to be installed
 nd_packages="\
+gnome-core mc evince bash-completion ntpdate file-roller gnome-utils \
+gnome-themes eog vim \
+software-center gdebi gedit-plugins gnome-media synaptic menu less \
 amide caret dicomnifti \
 fsl fsl-doc fslview fslview-doc \
 iceweasel \
 lipsia lipsia-doc via-bin \
 minc-tools nifti-bin odin praat \
 python-mvpa python-nifti python-pyepl \
-xmedcon"
+xmedcon \
+neurodebian-keyring
+"
 
 
-lh_config \
+lb config \
   --apt apt \
   --apt-recommends disabled \
   --architecture i386 \
-  --binary-images iso \
+  --archive-areas "main contrib non-free" \
+  --binary-images iso-hybrid \
   --binary-indices none \
   --bootstrap-flavour minimal \
-  --categories "main contrib non-free" \
   --debian-installer disabled \
   --distribution ${nd_basedist} \
-  --hostname debian \
-  --iso-application NeuroDebian \
-  --iso-publisher "NeuroDebian; http://neuro.debian.org" \
-  --mirror-bootstrap "http://debian.lcs.mit.edu/debian/" \
-  --packages-lists xfce \
+  --hostname neurodebian \
+  --iso-application "NeuroDebian Live" \
+  --iso-preparer "NeuroDebian v.6.0.0; http://neuro.debian.net" \
+  --iso-publisher "NeuroDebian project; http://neuro.debian.net" \
+  --mirror-bootstrap "http://debproxy:9999/debian/" \
   --packages "${nd_packages}" \
-  --username neuro \
+  --repositories "http://neuro.debian.net/debian" \
+  --username brain \
   --win32-loader enabled
 
-# add the NeuroDebian repository to the APT setup
-echo "deb http://neuro.debian.net/debian ${nd_basedist} main contrib non-free" \
-    > config/chroot_sources/neurodebian.chroot
-cp config/chroot_sources/neurodebian.chroot config/chroot_sources/neurodebian.binary
-# and the key
-gpg --export -a ${nd_key} > config/chroot_sources/neurodebian.chroot.gpg
-cp config/chroot_sources/neurodebian.chroot.gpg config/chroot_sources/neurodebian.binary.gpg
+  # --mirror-bootstrap "http://debian.lcs.mit.edu/debian/" \
+  # --packages-lists xfce \
 
+# May be
+# Deploy 1:1 copy of the running Live system
+# --debian-installer live
 
-# use NeuroDebian package cache
-if [ ! -e "cache/packages_bootstrap" ]; then
-  mkdir -p cache
-  ln -s ../../debian_aptcache/ cache/packages_bootstrap
-fi
+# Would be nice to have it utterly useful, but rescue is too bloated, may be
+# at least forensics
+# --packages-lists 
+
+## add the NeuroDebian repository to the APT setup
+#echo "deb http://neuro.debian.net/debian ${nd_basedist} main contrib non-free" \
+#    > config/chroot_sources/neurodebian.chroot
+#cp config/chroot_sources/neurodebian.chroot config/chroot_sources/neurodebian.binary
+## and the key
+#gpg --export -a ${nd_key} > config/chroot_sources/neurodebian.chroot.gpg
+#cp config/chroot_sources/neurodebian.chroot.gpg config/chroot_sources/neurodebian.binary.gpg
+#
+
+## use NeuroDebian package cache
+#if [ ! -e "cache/packages_bootstrap" ]; then
+#  mkdir -p cache
+#  ln -s ../../debian_aptcache/ cache/packages_bootstrap
+#fi
