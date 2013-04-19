aa26bd2b62184eb7d7356fb86d243c8550e6ff2d
Michael Hanke <michael.hanke@gmail.com>
2013-04-19 20:12:23 +0200

    Fix (C) year

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -64,7 +64,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Debian Neuroscience Package Repository'
-copyright = u'2009-2012, NeuroDebian Team <team@neuro.debian.net>'
+copyright = u'2009-2013, NeuroDebian Team <team@neuro.debian.net>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
