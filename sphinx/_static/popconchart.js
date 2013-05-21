68a98ac8a219dbe3f5dbeee91e2027c4b1f5dacf
Yaroslav Halchenko <debian@onerussian.com>
2013-05-20 23:02:28 -0400

    ENH: sort versions and provide colors to group debian/ubuntus in popcon plot
    now expanded makes sense -- we have more debians

--- sphinx/_static/popconchart.js
+++ sphinx/_static/popconchart.js
@@ -1,8 +1,22 @@
+var colors_debian = d3.scale.linear()
+	.domain([1.43, 1.58]).range(["white", "#dd1155"])
+var colors_ubuntu = d3.scale.linear()
+	.domain([1.43, 1.58]).range(["white", "#dd4814"])
+
+keyColor = function(d, i) {
+	var version = parseFloat(d.key.substr(0, 4))
+	if (d.key.indexOf("ubuntu") > 0)
+		return colors_ubuntu(version)
+	else
+		return colors_debian(version);
+}
+
 d3.json('/_files/nd_popconstats.json', function(data) {
   nv.addGraph(function() {
     chart = nv.models.stackedAreaChart()
                   .x(function(d) { return d[0] })
                   .y(function(d) { return d[1] })
+                  .color(keyColor)
                   .clipEdge(true);
 
     chart.stacked.style('stacked');
@@ -12,7 +26,7 @@ d3.json('/_files/nd_popconstats.json', function(data) {
             return d3.time.format('%d %b %Y')(new Date(d)) });
 
     chart.yAxis
-        .tickFormat(d3.format(',.2f'));
+        .tickFormat(d3.format(',d'));
 
     d3.select('#popconchart')
       .datum(data)
