4c7c700362d64130ab1fcbd0e8b48de7dbded61a
Yaroslav Halchenko <debian@onerussian.com>
2015-05-06 11:15:01 -0400

    saucy EOLed a while back, removing from cmdsettings

--- cmdsettings.sh
+++ cmdsettings.sh
@@ -7,7 +7,7 @@ buildplace="${cowbuilderroot}/build"
 
 # all currently supported dists
 allnddists="nd+debian-squeeze nd+debian-wheezy nd+debian-jessie nd+debian-stretch nd+debian-sid \
-            nd+ubuntu-lucid nd+ubuntu-precise nd+ubuntu-saucy nd+ubuntu-trusty nd+ubuntu-utopic nd+ubuntu-vivid"
+            nd+ubuntu-lucid nd+ubuntu-precise nd+ubuntu-trusty nd+ubuntu-utopic nd+ubuntu-vivid"
 alldists="$allnddists debian-squeeze debian-wheezy debian-jessie debian-stretch debian-sid"
 
 # default is debian
