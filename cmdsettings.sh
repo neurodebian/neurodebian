cd0aeac00126d7ab00b1f25500abd3289c96d7a6
Yaroslav Halchenko <debian@onerussian.com>
2015-11-05 21:08:56 -0500

    ENH: introduce wily (15.10) release, EOL lucid, plug for future xenial

--- cmdsettings.sh
+++ cmdsettings.sh
@@ -7,7 +7,7 @@ buildplace="${cowbuilderroot}/build"
 
 # all currently supported dists
 allnddists="nd+debian-wheezy nd+debian-jessie nd+debian-stretch nd+debian-sid \
-            nd+ubuntu-lucid nd+ubuntu-precise nd+ubuntu-trusty nd+ubuntu-vivid"
+            nd+ubuntu-precise nd+ubuntu-trusty nd+ubuntu-vivid nd+ubuntu-wily"
 alldists="$allnddists debian-wheezy debian-jessie debian-stretch debian-sid"
 
 # default is debian
